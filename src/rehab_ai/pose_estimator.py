7a7ab5495fba21c2447d8215bb46cbdca1600b10
SamMBurke <s8burke@uwaterloo.ca>
2026-06-25 18:33:24 -0400

    restructured the repo

--- src/rehab_ai/pose_estimator.py
+++ src/rehab_ai/pose_estimator.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import mediapipe as mp
 
-model_path = '/pose_landmarker_heavy.task'
+model_path = '../../models/pretrained/pose_landmarker_heavy.task'
 
 BaseOptions = mp.tasks.BaseOptions
 PoseLandmarker = mp.tasks.vision.PoseLandmarker
